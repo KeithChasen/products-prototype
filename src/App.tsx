0cf7ba947ef9ce487d9a6b75f1bf294ab4a562db
keithchasen <keithchasen89@gmail.com>
2024-07-16 12:26:14 +0200

    styling for header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ProductDetail from './pages/ProductDetail'
 import ProductList from './pages/ProductList';
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-      <>
+      <header>
             <Link to={'/'}>Products</Link>
-      </>
+      </header>
     )
   }
   
